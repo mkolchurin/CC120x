7cb5eb59ddd4716d04fb4df5b8e39c96a99900a6
mkolchurin <maxim.kolchurin@gmail.com>
2020-05-12 10:04:12 +0300

    first implement

--- Core/Inc/TISerialRadio.h
+++ Core/Inc/TISerialRadio.h
@@ -8,6 +8,21 @@
 #ifndef INC_TISERIALRADIO_H_
 #define INC_TISERIALRADIO_H_
 
+#include "stm32f4xx.h"
 
+uint8_t TISerialRadio_CLK();
+uint8_t TISerialRadio_readPin(void *GPIOx, uint16_t GPIO_Pin);
 
+void TISerialRadio_writePin(void *GPIOx, uint16_t GPIO_Pin, uint8_t PinState);
+
+uint8_t TISerialRadio_ReceiveSync(uint64_t syncword, uint32_t Timeout);
+uint8_t TISerialRadio_ReadByte();
+uint8_t TISerialRadio_Receive(uint8_t *buf, uint8_t buf_size, uint32_t Timeout);
+void TISerialRadio_init(void *RXport, uint16_t RXpin, void *TXport,
+		uint16_t TXpin, void *CLKport, uint16_t CLKpin);
+
+uint8_t TISerialRadio_Transmitt(uint8_t *buf, uint8_t buf_size,
+		uint32_t Timeout);
+uint8_t TISerialRadio_TransmittSync(uint64_t syncword, uint8_t *buf,
+		uint8_t buf_size, uint32_t Timeout);
 #endif /* INC_TISERIALRADIO_H_ */
--- Core/Src/TISerialRadio.c
+++ Core/Src/TISerialRadio.c
@@ -6,9 +6,6 @@
  */
 
 #include "TISerialRadio.h"
-//#include "stdint.h"
-#include "stm32f4xx.h"
-#include "stdbool.h"
 
 void *RX_Port;
 uint16_t RX_Pin;
@@ -16,56 +13,96 @@ void *TX_Port;
 uint16_t TX_Pin;
 void *CLK_Port;
 uint16_t CLK_Pin;
+uint8_t CLK = 0;
 
-bool TISerialRadio_readPin(void *GPIOx, uint16_t GPIO_Pin) {
-	return HAL_GPIO_ReadPin((GPIO_TypeDef*) GPIOx, GPIO_Pin);
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == CLK_Pin) {
+		CLK = 1;
+	}
 }
-
-void TISerialRadio_writePin(void *GPIOx, uint16_t GPIO_Pin, bool PinState) {
-	HAL_GPIO_WritePin((GPIO_TypeDef*) GPIOx, GPIO_Pin,
-			(GPIO_PinState) PinState);
-}
-
-uint8_t TISerialRadio_ReceiveSync(uint64_t syncword) {
-	uint64_t recsyncword = 0;
-	uint8_t bitpos = 0;
-
-	while (1) {
-		if (TISerialRadio_readPin(CLK_Port, CLK_Pin)) {
-			if (bitpos > 63 && recsyncword == syncword) {
-				bitpos = 0;
-				return 1;
-			}
-			bool pinstate = TISerialRadio_readPin(RX_Port, RX_Pin);
-			recsyncword = (recsyncword << 1) + pinstate;
-			bitpos++;
-		}
+uint8_t TISerialRadio_CLK() {
+	if (CLK == 1) {
+		CLK = 0;
+		return 1;
 	}
 	return 0;
 }
-
-uint8_t TISerialRadio_Receive(uint64_t syncword, uint8_t *buf, uint8_t size) {
-	GPIO_PinState pinstate = HAL_GPIO_ReadPin(rxPort, rxPin);
-
-	if (TISerialRadio_ReceiveSync(syncword)) {
-
-		buf[valIndex] += pinstate << (7 - counter++);
-		if (counter == 8) {
-			counter = 0;
-			valIndex++;
-
-			if (valIndex == length) {
-				syncreceive = 0;
-				HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
-				valIndex = 0;
-				transmitt = 1;
-				cc120x_WriteStrobe(STX);
-				cc120x_WriteStrobe(STX);
-			}
-		}
-	}
+uint8_t TISerialRadio_readPin(void *GPIOx, uint16_t GPIO_Pin) {
+	return HAL_GPIO_ReadPin(GPIOx, GPIO_Pin);
 }
 
+void TISerialRadio_writePin(void *GPIOx, uint16_t GPIO_Pin, uint8_t PinState) {
+	HAL_GPIO_WritePin(GPIOx, GPIO_Pin, PinState);
+}
+
+uint8_t TISerialRadio_ReadByte() {
+	uint8_t val = 0;
+	for (uint8_t bitCounter = 0; bitCounter != 8;) {
+		if (TISerialRadio_CLK() == 1) {
+			uint8_t pinstate = TISerialRadio_readPin(RX_Port, RX_Pin);
+			val = (val << 1) + pinstate;
+			bitCounter++;
+		}
+	}
+	return val;
+}
+
+
+uint8_t TISerialRadio_ReceiveSync(uint64_t syncword, uint32_t Timeout) {
+	uint64_t recsyncword = 0;
+	uint8_t bitpos = 0;
+	uint32_t tickstart = HAL_GetTick();
+	while ((bitpos < 63) || (recsyncword != syncword)) {
+		if (CLK == 1) {
+			CLK = 0;
+			uint8_t pinstate = TISerialRadio_readPin(RX_Port, RX_Pin);
+			recsyncword = (recsyncword << 1) + pinstate;
+			bitpos++;
+		}
+		if ((HAL_GetTick() - tickstart) > Timeout)
+			return 0;
+	}
+	bitpos = 0;
+	return (HAL_GetTick());
+}
+
+
+uint8_t TISerialRadio_Receive(uint8_t *buf, uint8_t buf_size, uint32_t Timeout) {
+	uint32_t tickstart = HAL_GetTick();
+	for (int i = 0; i < buf_size; i++)
+		buf[i] = TISerialRadio_ReadByte();
+	if ((HAL_GetTick() - tickstart) > Timeout)
+		return 0;
+	return 1;
+}
+
+uint8_t TISerialRadio_TransmittSync(uint64_t syncword, uint8_t *buf,
+		uint8_t buf_size, uint32_t Timeout) {
+	uint8_t *sync = (uint8_t*) &syncword;
+	TISerialRadio_Transmitt(sync, sizeof(syncword), Timeout);
+	return TISerialRadio_Transmitt(buf, buf_size, Timeout);
+
+}
+uint8_t TISerialRadio_Transmitt(uint8_t *buf, uint8_t buf_size,
+		uint32_t Timeout) {
+	uint32_t tickstart = HAL_GetTick();
+	uint8_t bitpos = 0;
+	for (int i = 0; i < buf_size;) {
+		if (CLK == 1) {
+			CLK = 0;
+			for (uint8_t bitpos = 0; bitpos != 8; bitpos++) {
+				uint8_t state = ((buf[i] & (0x80 >> bitpos)) >> (7 - bitpos));
+				TISerialRadio_writePin(TX_Port, TX_Pin, state);
+			}
+			i++;
+
+			if ((HAL_GetTick() - tickstart) > Timeout)
+				return 0;
+		}
+	}
+	return 1;
+
+}
 void TISerialRadio_init(void *RXport, uint16_t RXpin, void *TXport,
 		uint16_t TXpin, void *CLKport, uint16_t CLKpin) {
 	RX_Port = RXport;
